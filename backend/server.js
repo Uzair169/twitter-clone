c2f4572ec6f6f0bb2544edba7e4d77c4dd1a9527
Uzair169 <96523038+Uzair169@users.noreply.github.com>
2024-09-21 18:40:17 +0500

    Auth Routes and Controllers

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,17 @@ import dotenv from "dotenv"
 
 import authRoutes from './routes/auth.routes.js'
 import connectMongoDB from "./db/connectMongoDB.js"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(express.json())   //To parse req.body
+app.use(express.urlencoded({ extended: true })) //To parse form data
+
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 
