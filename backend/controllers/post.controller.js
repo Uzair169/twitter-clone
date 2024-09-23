6e6c68af8f8a5f032526763567f57446836eefe7
Uzair169 <96523038+Uzair169@users.noreply.github.com>
2024-09-23 12:40:44 +0500

    1- Post Routes & Controllers completed

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -176,3 +176,52 @@ export const getLikedPosts = async (req, res) => {
         console.log("Error in getLikedPosts Controller", error)
     }
 }
+
+export const getFollowingPosts = async (req, res) => {
+    try {
+        const userId = req.user._id
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({ error: "User not found" })
+        }
+
+        const following = user.following
+
+        const feedPosts = await Post.find({ user: { $in: following } }).sort({ createdAt: -1 }).populate({
+            path: "user",
+            select: "-password",
+        }).populate({
+            path: "comments.user",
+            select: "-password"
+        })
+
+        return res.status(200).json(feedPosts)
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" })
+        console.log("Error in getFollowingPosts Controller", error)
+    }
+}
+
+export const getUserPosts = async (req, res) => {
+    try {
+        const { username } = req.params
+
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.status(404).json({ error: "User not found" })
+        }
+
+        const posts = await Post.find({ user: user._id }).sort({ createdAt: -1 }).populate({
+            path: "user",
+            select: "-password",
+        }).populate({
+            path: "comments.user",
+            select: "-password"
+        })
+
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" })
+        console.log("Error in getUserPosts Controller", error)
+    }
+}
