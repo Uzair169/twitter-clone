d814d46bb4533ad1894d18a6990cd160419df062
Uzair169 <96523038+Uzair169@users.noreply.github.com>
2024-09-28 01:55:34 +0500

    UI design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-// import './App.css'
+import { Route, Routes } from "react-router-dom"
+import HomePage from './pages/home/HomePage.jsx'
+import LoginPage from './pages/auth/login/LoginPage.jsx'
+import SignUpPage from './pages/auth/signup/SignUpPage.jsx'
+import Sidebar from './components/common/Sidebar.jsx'
+import RightPanel from "./components/common/RightPanel.jsx"
+import NotificationPage from './pages/notification/NotificationPage.jsx'
+import ProfilePage from "./pages/profile/profilePage.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route path="/notifications" element={<NotificationPage />}></Route>
+        <Route path="/profile/:username" element={<ProfilePage />}></Route>
+      </Routes>
+      <RightPanel />
+    </div>
   )
 }
 
