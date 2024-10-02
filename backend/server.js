0d88bd5d153a5dc1b01ea3c6552d596c60d4c953
Uzair169 <96523038+Uzair169@users.noreply.github.com>
2024-10-02 13:42:17 +0500

    4- Push to Render for Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
-import {v2} from 'cloudinary'
+import { v2 } from 'cloudinary'
+import path from "path"
 
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
@@ -20,8 +21,9 @@ v2.config({
 
 const app = express()
 const port = process.env.PORT || 5000
+const __dirname = path.resolve()
 
-app.use(express.json({limit: '5mb'}))   //To parse req.body
+app.use(express.json({ limit: '5mb' }))   //To parse req.body
 app.use(express.urlencoded({ extended: true })) //To parse form data
 
 app.use(cookieParser())
@@ -30,6 +32,14 @@ app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/notifications", notificationRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
